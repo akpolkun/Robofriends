c9ff992ac3e5c4aa49d89371962f7760aae48fb9
Thariq Agfi Hermawan <thoriq.aghfi60@gmail.com>
2022-06-23 09:42:11 +0700

    adding node_modules and change root

--- src/index.js
+++ src/index.js
@@ -5,11 +5,16 @@ import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
+//Another kind to createRoot :
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 reportWebVitals();
