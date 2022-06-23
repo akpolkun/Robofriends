07a6b60cc6c81533dc60829b6960c47c08c0d9e6
Akpol <92865110+akpolkun@users.noreply.github.com>
2022-06-23 08:28:24 +0700

    first upload

---
+++ src/Robot.js
@@ -0,0 +1,44 @@
+export const Robot = [
+    {
+        id: 1,
+        name: 'Leanne Grahan',
+        username: 'Bret',
+        email: 'sincere@ampril.biz',
+    },
+    {
+        id: 2,
+        name: 'Herri Hermawan',
+        username: 'Her',
+        email: 'Heri@gmail.com',
+    },
+    {
+        id: 3,
+        name: 'Siti Rachma',
+        username: 'Rahma',
+        email: 'sitirachma@ampril.biz',
+    },
+    {
+        id: 4,
+        name: 'Athariq',
+        username: 'Atha',
+        email: 'atha@ampril.biz',
+    },
+    {
+        id: 5,
+        name: 'Zahran Ardhi',
+        username: 'Ardhi',
+        email: 'zahran@ampril.biz',
+    },
+    {
+        id: 6,
+        name: 'Tahrqi H',
+        username: 'Tharqi',
+        email: 'thariq@ampril.biz',
+    },
+    {
+        id: 7,
+        name: 'Thariq AGFI',
+        username: 'Thariq',
+        email: 'Agfi@ampril.biz',
+    },
+]
